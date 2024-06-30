2c662c7f89c54eba5be1f88d5dcc6f27807dae3a
ianramy <imwagore@gmail.com>
2024-06-30 05:44:48 +0300

    origin/main

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const contactButton = document.getElementById("contact-button");
-    contactButton.addEventListener("click", function() {
-        alert("Thank you for your interest! I will get back to you soon.");
-    });
-});
